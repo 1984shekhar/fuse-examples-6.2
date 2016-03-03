f2dd668673cda72f74c1c0ee538d5833d2512821
cpandey <shekhar.csp84@yahoo.co.in>
2016-03-03 13:50:38 +0530

    camel-sqs test-case

---
+++ Camel-Amazon-Sqs-Test/src/main/java/com/example/AllAttributesList.java
@@ -0,0 +1,12 @@
+package com.example;
+
+
+import java.util.ArrayList;
+
+public class AllAttributesList extends ArrayList<String> {
+
+    public AllAttributesList() {
+        super();
+        add("All");
+    }
+}
