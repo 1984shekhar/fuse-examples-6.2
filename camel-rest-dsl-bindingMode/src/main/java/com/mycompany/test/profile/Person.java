db04881ae265d352c3c46712686e1fcdfc97600c
1984shekhar <shekhar.csp84@yahoo.co.in>
2016-07-14 08:19:35 +0530

    REST dsl bindingMode example

---
+++ camel-rest-dsl-bindingMode/src/main/java/com/mycompany/test/profile/Person.java
@@ -0,0 +1,21 @@
+package com.mycompany.test.profile;
+
+public class Person {
+	
+private String name;
+private String id;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+
+
+}
