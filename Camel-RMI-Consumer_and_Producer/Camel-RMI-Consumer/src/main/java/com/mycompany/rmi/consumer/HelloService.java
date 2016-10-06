d3bd35bc82d70f067c7b0bafb19708241fb6c6ea
1984shekhar <shekhar.csp84@yahoo.co.in>
2016-10-06 14:56:05 +0530

    separate bundl for rmi consumer and rmi producer

---
+++ Camel-RMI-Consumer_and_Producer/Camel-RMI-Consumer/src/main/java/com/mycompany/rmi/consumer/HelloService.java
@@ -0,0 +1,16 @@
+package com.mycompany.rmi.consumer;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Interface for the service to expose. Notice we must extend {@link Remote} which
+ * is required when using RMI.
+ *
+ * @version 
+ */
+public interface HelloService extends Remote {
+
+    String hello(String name, String lastName) throws RemoteException;
+
+}
